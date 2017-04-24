3b746e10b7ca46534ed32afed41d3211f0f6c4fd
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-04-24 17:52:26 +0200

    Sounds and music

--- src/js/loadingScene.js
+++ src/js/loadingScene.js
@@ -17,6 +17,8 @@ var loadingScene = {
     .add('tile_sand', 'assets/images/tile_sand.png')
 
     .add('button_hover', 'assets/images/button_hover.png')
+    
+    .add('speaker', 'assets/images/speaker.png')
 
     .add('flare_super', 'assets/images/flare_super.png')
     .add('flare_disaster', 'assets/images/flare_disaster.png')
